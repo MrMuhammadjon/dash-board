e50a48dcc3488acd40658220db4afb365e99370e
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-03-19 15:03:39 +0500

    js

---
+++ script.js
@@ -0,0 +1,31 @@
+let sideBar = document.getElementById('side-bar')
+
+function OpenClose() {
+    sideBar.classList.toggle('close')
+}
+
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 945) {
+        sideBar.classList.add('close')
+    } else {
+        sideBar.classList.remove('close')
+    }
+});
+
+
+let slideList = document.querySelectorAll('.side-bar .side-menu li a:not(.logout)')
+slideList.forEach(item => {
+    const li = item.parentElement
+    item.addEventListener('click', () => {
+        slideList.forEach(i => {
+            i.parentElement.classList.remove('active')
+        })
+        li.classList.add('active')
+
+    })
+
+})
+
+
+let togger = document.getElementById('theme-toogle')
